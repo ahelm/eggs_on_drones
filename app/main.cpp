713cd72fc766662780268a3c0c867cb9669cb448
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-04-03 23:54:23 +0100

    move to new modern & modular structure

---
+++ app/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "drone.h"
+
+int main()
+{
+  std::cout << "Hello from main!" << std::endl;
+  drone_say_hello();
+}
---
+++ src/drone.cpp
@@ -0,0 +1,7 @@
+#include "drone.h"
+#include <iostream>
+
+void drone_say_hello()
+{
+  std::cout << "drone says hello" << std::endl;
+}
