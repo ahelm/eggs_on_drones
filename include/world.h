69651f17909865634d4023c0bcbb83418feef653
Anton Helm <anton.helm@tecnico.ulisboa.pt>
2021-04-04 12:15:46 +0100

    create basic layout for world

--- include/world.h
+++ include/world.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <vector>
+#include "drone.h"
+
+namespace dronenav
+{
+  class World
+  {
+  private:
+    int width_;
+    int height_;
+    std::vector<Drone> drones_;
+  public:
+    int width();
+    int height();
+    std::vector<Drone> drones();
+  };
+} // namespace dronenav
